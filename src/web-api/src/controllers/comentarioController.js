410c92a9a85a3a24beb50fd3a2c0641a68c3d371
Patrick Douglas <pdouglas.s@hotmail.com>
2023-09-29 19:55:07 -0300

    Adicionando  controller comentario

---
+++ src/web-api/src/controllers/comentarioController.js
@@ -0,0 +1,84 @@
+const ComentarioModel = require("../models/Comentario");
+
+const comentarioController = {
+  create: async (req, res) => {
+    try {
+      const comentario = {
+        descricao: req.body.descricao,
+      };
+
+      const response = await ComentarioModel.create(comentario);
+
+      res.status(201).json({ response, msg: "Comentario registrado!" });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  getAll: async (req, res) => {
+    try {
+      const comentarios = await ComentarioModel.find();
+
+      res.status(200).json(comentarios);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  get: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const comentario = await ComentarioModel.findById(id);
+
+      !comentario
+        ? res.status(404).json({ msg: "Comentario não encontrado!" })
+        : res.status(200).json(comentario);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  delete: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const comentario = await ComentarioModel.findById(id);
+
+      if (!comentario) {
+        res.status(404).json({ msg: "Comentario não encontrado!" });
+        return;
+      }
+
+      const comentarioDeletado = await ComentarioModel.findByIdAndDelete(id);
+      res.status(200).json({
+        comentarioDeletado,
+        msg: `Registro de ${comentarioDeletado.descricao} excluído com sucesso!`,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  update: async (req, res) => {
+    try {
+      const id = req.params.id;
+
+      const comentario = {
+        descricao: req.body.descricao,
+      };
+
+      const comentarioAtualizado = await ComentarioModel.findByIdAndUpdate(
+        id,
+        comentario
+      );
+
+      if (!comentarioAtualizado) {
+        res.status(404).json({ msg: "Comentario não encontrado!" });
+        return;
+      }
+
+      res
+        .status(200)
+        .json({ comentario, msg: "Comentario atualizado com sucesso!" });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
+
+module.exports = comentarioController;
--- src/web-api/src/routes/router.js
+++ src/web-api/src/routes/router.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
 const autonomosRouter = require("./autonomos");
+const comentariosRouter = require("./comentarios");
 
 router.use("/", autonomosRouter);
+router.use("/", comentariosRouter);
 
 module.exports = router;
