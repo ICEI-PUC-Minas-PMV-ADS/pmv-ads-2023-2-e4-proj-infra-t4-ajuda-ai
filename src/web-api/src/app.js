5a6c82b60808d0d24835c0b05f4820c41e959da5
Marcusvinimf <marcusvinimf@gmail.com>
2023-10-08 14:58:57 -0300

    inserindo o arquivo vercel para deploy de backend

--- src/web-api/src/app.js
+++ src/web-api/src/app.js
@@ -6,18 +6,15 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+const apresentation = require("./routes/routePresentation");
+apresentation(app, port);
+
 const conn = require("./db/conn");
 conn();
 
 const routes = require("./routes/router");
 app.use("/api", routes);
 
-
-app.get("/", (req, res) => {
-  res.send(
-    `Servidor está funcionando!!! <br/><br/><br/> Teste de perfis autônomos >>> <a href="http://localhost:${port}/api/autonomos">Teste de Perfis Autônomos</a>`
-  );
-});
 
 app.listen(port, () =>
   console.log(`Servidor rodando na rota http://localhost:${port}`)
